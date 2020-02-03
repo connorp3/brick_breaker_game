6b1578d4f88359528f4bc9ebf84ba75308b2944f
Connor Penny <cgp19@duke.edu>
2020-02-03 15:12:42 -0500

    Extracted class to create scene for breakout

--- src/breakout/SceneCreation.java
+++ src/breakout/SceneCreation.java
@@ -2,55 +2,27 @@ package breakout;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.animation.TranslateTransition;
-import javafx.application.Application;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.*;
-import javafx.scene.transform.Rotate;
-import javafx.scene.transform.Scale;
-import javafx.scene.transform.Translate;
-import javafx.stage.Stage;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.shape.Shape;
 import javafx.util.Duration;
 
-import javafx.scene.input.KeyEvent;
-import javafx.event.EventHandler;
-
-/**
- * Feel free to completely change this code or delete it entirely. 
- */
-public class Main extends Application {
-    /**
-     * Start of the program.
-     */
-    public static final int FRAMES_PER_SECOND = 60;
-    public static final double SECOND_DELAY = 1.0/FRAMES_PER_SECOND;
+public class SceneCreation {
     private Paddle myPaddle;
     private Ball myBall;
-    private Scene myScene;
-    public static void main (String[] args) {
-        Application.launch();
-    }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+    public SceneCreation() {
         myPaddle = new Paddle();
         myBall = new Ball();
         Group root = new Group(myPaddle.getShape());
         ObservableList gameElements = root.getChildren();
         gameElements.add(myBall.getShape());
         Scene scene = new Scene(root, 550, 450);
-        myScene = scene;
-
         scene.setOnKeyPressed(keyEventHandler(myPaddle));
-        SceneCreation sceneCreation = new SceneCreation();
-        primaryStage.setTitle("Breakout");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
 
         Timeline myAnimation = new Timeline();
         KeyFrame frame = new KeyFrame(Duration.seconds(SECOND_DELAY), e -> update(SECOND_DELAY, myScene));
