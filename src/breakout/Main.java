a5e8852da52540d0ef764ce37085123e5453d30c
Connor Penny <cgp19@duke.edu>
2020-02-02 22:46:28 -0500

    Created a rectangle that will eventually be the paddle

--- src/breakout/Main.java
+++ src/breakout/Main.java
@@ -1,6 +1,13 @@
 package breakout;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
@@ -16,6 +23,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Rectangle paddle = new Rectangle(250, 430, 50, 10);
+        paddle.setFill(Color.RED);
+        Group root = new Group(paddle);
+        Scene scene = new Scene(root, 550, 450);
+
+
+        primaryStage.setTitle("Breakout");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
